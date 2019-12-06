61a3ff2f92365f22266d1ac0417cf757f6459fa2
Gergely Fodor <fodgeri@outlook.com>
2019-12-06 11:22:03 +0100

    Intial

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PasswordHasher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hash = SecurePasswordHasher.Hash("<Your Password>");
+
+            Console.WriteLine($"Hash: {hash}");
+
+            // Verify
+            var result = SecurePasswordHasher.Verify("<Your Password>", hash);
+
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
